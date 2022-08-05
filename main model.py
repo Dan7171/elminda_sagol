7f9912dc742c58dbf2ae7dda873f0a4c048a988b
Dan <evrondan99@gmail.com>
2022-08-05 17:30:08 +0300

    hopefuly not the "hello" files

--- main model.py
+++ main model.py
@@ -1,2 +1,9 @@
-print("hello")
-print("hello2")
+import pandas as pd
+import numpy as np
+import datetime as dt
+import sklearn
+
+clinical_data = pd.read_excel('files\BY_clinical_data_Sagol.xlsx')
+eeg_data = pd.read_csv('files\zscores_Sagol.csv')
+print(clinical_data.sample)
+print(eeg_data.sample)
